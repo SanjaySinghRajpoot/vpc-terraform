d45a19abf3e4f429344f5e62ad60f49efa04db10
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2023-10-26 16:00:39 +0530

    feat: vpc module setup

---
+++ terraform-vpc/modules/vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "vpc_cidr" {
+  type        = string
+  default     = ""
+  description = "VPC CIDR Range"
+}
+
+variable "subnet_cidr" {
+    description = "Subnet CIDRS"
+    type = list(string)
+}
+
+variable "subnet_names" {
+    description = "Subnet names"
+    type = list(string)
+    default = ["PublicSubnet1", "PublicSubnet2"]
+}
